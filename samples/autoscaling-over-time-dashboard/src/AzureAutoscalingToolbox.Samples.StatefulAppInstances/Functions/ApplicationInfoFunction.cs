cd4b0e6b43ec80458ee6d33795d90bfc780e7e70
Tom Kerkhove <kerkhove.tom@gmail.com>
2021-07-02 22:17:31 +0200

    Allow users to get instance count
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- samples/autoscaling-over-time-dashboard/src/AzureAutoscalingToolbox.Samples.StatefulAppInstances/Functions/ApplicationInfoFunction.cs
+++ samples/autoscaling-over-time-dashboard/src/AzureAutoscalingToolbox.Samples.StatefulAppInstances/Functions/ApplicationInfoFunction.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using AzureAutoscalingToolbox.Samples.StatefulAppInstances.Entities;
+using AzureAutoscalingToolbox.Samples.StatefulAppInstances.Entities.Identifiers;
 using AzureAutoscalingToolbox.Samples.StatefulAppInstances.Functions.Foundation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,20 +12,52 @@ namespace AzureAutoscalingToolbox.Samples.StatefulAppInstances.Functions
 {
     public class ApplicationInfoFunction : HttpFunction
     {
-        [FunctionName("application-info-instance-count")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "v1/app/instances")] HttpRequest request,
+        [FunctionName("generic-application-info")]
+        public async Task<IActionResult> GetInfoForGenericApp([HttpTrigger(AuthorizationLevel.Function, "get", Route = "v1/apps/generic/instances")] HttpRequest request,
             [DurableClient] IDurableEntityClient durableEntityClient)
         {
             // Determine the application id
-            var applicationId = request.Query["appId"];
-            if (string.IsNullOrWhiteSpace(applicationId))
+            var rawEntityId = request.Query["entityId"];
+            if (string.IsNullOrWhiteSpace(rawEntityId))
             {
                 return BadRequest("No application id was specified");
             }
 
             // Get the current state of our application entity
-            var entityId = new EntityId(GenericApplicationEntity.EntityName, applicationId);
+            var entityId = new EntityId(GenericApplicationEntity.EntityName, rawEntityId);
             var appIdentity = await durableEntityClient.ReadEntityStateAsync<GenericApplicationEntity>(entityId);
+
+            // If it doesn't exist, return HTTP 404
+            if (appIdentity.EntityExists == false)
+            {
+                return NotFound();
+            }
+
+            // If it does exist, return HTTP 200 with instance count
+            return OkWithPayload(new { appIdentity.EntityState.InstanceCount });
+        }
+
+        [FunctionName("kubernetes-application-info")]
+        public async Task<IActionResult> GetInfoForKubernetesApp([HttpTrigger(AuthorizationLevel.Function, "get", Route = "v1/apps/kubernetes/instances")] HttpRequest request,
+            [DurableClient] IDurableEntityClient durableEntityClient)
+        {
+            // Determine the application id
+            var deploymentName = request.Query["deploymentName"];
+            if (string.IsNullOrWhiteSpace(deploymentName))
+            {
+                return BadRequest("No deployment name was specified");
+            }
+
+            // Determine the application id
+            var namespaceName = request.Query["namespaceName"];
+            if (string.IsNullOrWhiteSpace(namespaceName))
+            {
+                return BadRequest("No namespace name was specified");
+            }
+
+            // Get the current state of our application entity
+            var entityId = new KubernetesEntityIdentifier(deploymentName, namespaceName).GetEntityId();
+            var appIdentity = await durableEntityClient.ReadEntityStateAsync<KubernetesApplicationEntity>(entityId);
 
             // If it doesn't exist, return HTTP 404
             if (appIdentity.EntityExists == false)
